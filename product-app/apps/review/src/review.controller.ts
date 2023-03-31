61692d01af7309ed32f2ff197054bb14db132d9e
marcellodinizr <marcellodinizrocha@gmail.com>
2023-03-31 21:26:44 +0100

    feat: adding handleProductCreated

--- product-app/apps/review/src/review.controller.ts
+++ product-app/apps/review/src/review.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { RmqService } from 'y/common';
+
 import { ReviewService } from './review.service';
 
 @Controller()
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly rmqService: RmqService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.reviewService.getHello();
+  }
+
+  @EventPattern('product_created')
+  async handleProductCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    this.reviewService.bill(data);
+    this.rmqService.ack(context);
   }
 }
