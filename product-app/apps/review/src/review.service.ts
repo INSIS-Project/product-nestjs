04fce74fcae0be47bcb2e3d13ca1dcd717d2c46c
marcellodinizr <marcellodinizrocha@gmail.com>
2023-03-31 21:33:48 +0100

    feat: adding logger in review.service

--- product-app/apps/review/src/review.service.ts
+++ product-app/apps/review/src/review.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class ReviewService {
+  private readonly logger = new Logger(ReviewService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  bill(data: any) {
+    this.logger.log('Review...', data);
   }
 }
