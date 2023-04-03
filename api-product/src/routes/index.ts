79a61dead3003f4714cb0cd5a958b4eec8023122
marcellodinizr <marcellodinizrocha@gmail.com>
2023-04-03 18:12:38 +0100

    general updates

--- api-product/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { productsRoutes } from './products.routes';
-
-const router = Router();
-
-router.use('/products', productsRoutes);
-
-export { router };
